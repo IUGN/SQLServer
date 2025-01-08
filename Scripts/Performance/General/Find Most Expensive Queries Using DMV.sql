1be65edd773c3421436fd8a8f81d56312f7ffcfb
IUGN <yugkz@rambler.ru>
2025-01-08 14:08:28 +0500

    First

--- Scripts/Performance/General/Find Most Expensive Queries Using DMV.sql
+++ Scripts/Performance/General/Find Most Expensive Queries Using DMV.sql
@@ -1,40 +1,45 @@
-SELECT TOP 10 SUBSTRING(qt.TEXT, (qs.statement_start_offset/2)+1,
+SELECT TOP 10
+    SUBSTRING(qt.TEXT, (qs.statement_start_offset/2)+1,
 ((CASE qs.statement_end_offset
 WHEN -1 THEN DATALENGTH(qt.TEXT)
 ELSE qs.statement_end_offset
 END - qs.statement_start_offset)/2)+1),
-qs.execution_count,
-qs.total_logical_reads, qs.last_logical_reads,
-qs.total_logical_writes, qs.last_logical_writes,
-qs.total_worker_time,
-qs.last_worker_time,
-qs.total_elapsed_time/1000000 total_elapsed_time_in_S,
-qs.last_elapsed_time/1000000 last_elapsed_time_in_S,
-qs.last_execution_time,
-qp.query_plan
+    qs.execution_count,
+    qs.total_logical_reads, qs.last_logical_reads,
+    qs.total_logical_writes, qs.last_logical_writes,
+    qs.total_worker_time,
+    qs.last_worker_time,
+    qs.total_elapsed_time/1000000 total_elapsed_time_in_S,
+    qs.last_elapsed_time/1000000 last_elapsed_time_in_S,
+    qs.last_execution_time,
+    qp.query_plan
 FROM sys.dm_exec_query_stats qs
 CROSS APPLY sys.dm_exec_sql_text(qs.sql_handle) qt
 CROSS APPLY sys.dm_exec_query_plan(qs.plan_handle) qp
-ORDER BY qs.total_logical_reads DESC -- logical reads
+ORDER BY qs.total_logical_reads DESC
+-- logical reads
 -- ORDER BY qs.total_logical_writes DESC -- logical writes
 -- ORDER BY qs.total_worker_time DESC -- CPU time
 
 --Источник <https://blog.sqlauthority.com/2010/05/14/sql-server-find-most-expensive-queries-using-dmv/> 
 
-SELECT TOP(50) qs.execution_count AS [Execution Count],
-(qs.total_logical_reads)*8/1024.0 AS [Total Logical Reads (MB)],
-(qs.total_logical_reads/qs.execution_count)*8/1024.0 AS [Avg Logical Reads (MB)],
-(qs.total_worker_time)/1000.0 AS [Total Worker Time (ms)],
-(qs.total_worker_time/qs.execution_count)/1000.0 AS [Avg Worker Time (ms)],
-(qs.total_elapsed_time)/1000.0 AS [Total Elapsed Time (ms)],
-(qs.total_elapsed_time/qs.execution_count)/1000.0 AS [Avg Elapsed Time (ms)],
-qs.creation_time AS [Creation Time]
-,t.text AS [Complete Query Text], qp.query_plan AS [Query Plan]
+SELECT TOP(50)
+    qs.execution_count AS [Execution Count],
+    (qs.total_logical_reads)*8/1024.0 AS [Total Logical Reads (MB)],
+    (qs.total_logical_reads/qs.execution_count)*8/1024.0 AS [Avg Logical Reads (MB)],
+    (qs.total_worker_time)/1000.0 AS [Total Worker Time (ms)],
+    (qs.total_worker_time/qs.execution_count)/1000.0 AS [Avg Worker Time (ms)],
+    (qs.total_elapsed_time)/1000.0 AS [Total Elapsed Time (ms)],
+    (qs.total_elapsed_time/qs.execution_count)/1000.0 AS [Avg Elapsed Time (ms)],
+    qs.creation_time AS [Creation Time]
+, t.text AS [Complete Query Text], qp.query_plan AS [Query Plan]
 FROM sys.dm_exec_query_stats AS qs WITH (NOLOCK)
 CROSS APPLY sys.dm_exec_sql_text(plan_handle) AS t
 CROSS APPLY sys.dm_exec_query_plan(plan_handle) AS qp
 WHERE t.dbid = DB_ID()
-ORDER BY qs.execution_count DESC OPTION (RECOMPILE);-- frequently ran query
+ORDER BY qs.execution_count DESC
+OPTION
+(RECOMPILE);-- frequently ran query
 -- ORDER BY [Total Logical Reads (MB)] DESC OPTION (RECOMPILE);-- High Disk Reading query
 -- ORDER BY [Avg Worker Time (ms)] DESC OPTION (RECOMPILE);-- High CPU query
 -- ORDER BY [Avg Elapsed Time (ms)] DESC OPTION (RECOMPILE);-- Long Running query
--- Scripts/Performance/Memory/DMV sys.dm_exec_query_memory_grants.sql
+++ Scripts/Performance/Memory/DMV sys.dm_exec_query_memory_grants.sql
@@ -4,7 +4,9 @@
 	• Проверьте запросы, ожидающие 1 для получения разрешений, с помощью sys.dm_exec_query_memory_grants:
 */
 --Find all queries waiting in the memory queue  
-SELECT * FROM sys.dm_exec_query_memory_grants WHERE grant_time IS NULL;
+SELECT *
+FROM sys.dm_exec_query_memory_grants
+WHERE grant_time IS NULL;
 
 /*В этом случае типом ожидания, как правило, является RESOURCE_SEMAPHORE. Для получения дополнительной информации см. sys.dm_os_wait_stats (Transact-SQL).
 	• Кэш поиска запросов с выделением памяти с помощью sys.dm_exec_cached_plans (Transact-SQL) и sys.dm_exec_query_plan (Transact-SQL)
@@ -12,19 +14,19 @@ SELECT * FROM sys.dm_exec_query_memory_grants WHERE grant_time IS NULL;
 
 -- retrieve every query plan from the plan cache  
 USE master;  
-GO  
-SELECT * 
+GO
+SELECT *
 FROM sys.dm_exec_cached_plans cp 
 CROSS APPLY sys.dm_exec_query_plan(cp.plan_handle);  
-GO  
-	/*• Если подозревается беглый запрос, изучите Showplan в столбце query_planиз sys.dm_exec_query_plan и пакет text запроса из sys.dm_exec_sql_text. 
+GO
+/*• Если подозревается беглый запрос, изучите Showplan в столбце query_planиз sys.dm_exec_query_plan и пакет text запроса из sys.dm_exec_sql_text. 
     Дополнительные сведения о том, как в настоящее время выполняются запросы с большим объемом памяти, используя sys.dm_exec_requests.
 */
 
 --Active requests with memory grants
 SELECT
---Session data 
-  s.[session_id], s.open_transaction_count
+	--Session data 
+	s.[session_id], s.open_transaction_count
 --Memory usage
 , r.granted_query_memory, mg.grant_time, mg.requested_memory_kb, mg.granted_memory_kb, mg.required_memory_kb, mg.used_memory_kb, mg.max_used_memory_kb     
 --Query 
@@ -33,14 +35,14 @@ SELECT
 , s.last_request_start_time, s.last_request_end_time, s.reads, s.writes, s.logical_reads, session_status = s.[status], request_status = r.status
 --Session connection information
 , s.host_name, s.program_name, s.login_name, s.client_interface_name, s.is_user_process
-FROM sys.dm_exec_sessions s 
-LEFT OUTER JOIN sys.dm_exec_requests AS r 
-    ON r.[session_id] = s.[session_id]
-LEFT OUTER JOIN sys.dm_exec_query_memory_grants AS mg 
-    ON mg.[session_id] = s.[session_id]
+FROM sys.dm_exec_sessions s
+	LEFT OUTER JOIN sys.dm_exec_requests AS r
+	ON r.[session_id] = s.[session_id]
+	LEFT OUTER JOIN sys.dm_exec_query_memory_grants AS mg
+	ON mg.[session_id] = s.[session_id]
 OUTER APPLY sys.dm_exec_sql_text (r.[sql_handle]) AS t
 OUTER APPLY sys.dm_exec_input_buffer(s.[session_id], NULL) AS ib 
-OUTER APPLY sys.dm_exec_query_plan (r.[plan_handle]) AS qp 
+OUTER APPLY sys.dm_exec_query_plan (r.[plan_handle]) AS qp
 WHERE mg.granted_memory_kb > 0
 ORDER BY mg.granted_memory_kb desc, mg.requested_memory_kb desc;
 GO
